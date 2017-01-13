d0b3d2b6b9d3ceca0359b1fd0deaaee01771fb75
Frank <616180981@qq.com>
2017-01-13 10:30:01 +0800

    zmyfrank

--- js/controllers.js
+++ js/controllers.js
@@ -13,7 +13,9 @@ mainCtrl.controller('HelloCtrl', ['$scope',
 /**
  * 这里接着往下写，如果控制器的数量非常多，需要分给多个开发者，可以借助于grunt来合并代码
  */
-
+mainCtrl.controller("list",function ($scope) {
+    $scope.html = 1;
+})
 
 
 
